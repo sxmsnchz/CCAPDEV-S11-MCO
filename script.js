ad54130b4c0cabe7a47bb9c81f0221d5fa51bf7d
larruuhh5 <160387688+larruuhh5@users.noreply.github.com>
2026-02-05 14:22:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const isLoggedIn = true;
 const userRole = "org"; // "student" "org" "admin"
 const currentUser = "org-au";
 
+//FOR ORG PAGE
 document.addEventListener("DOMContentLoaded", () => {
 
     //lightbox
@@ -229,3 +230,74 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+// END OF ORG PAGE
+
+// FOR MENU
+
+
+const profileBtn = document.getElementById("profileBtn");
+const profileDropdown = document.getElementById("profileDropdown");
+const profileMenu = document.getElementById("profileMenu");
+
+function renderProfileMenu() {
+    profileDropdown.innerHTML = "";
+
+    if (isLoggedIn) {
+        profileDropdown.innerHTML= `
+        <li><a href="profile.html">Profile</a></li>
+        <li><button type="button" id="signOutBtn">Sign Out</button></li>
+        `;
+
+        document.getElementById("signOutBtn").addEventListener("click", () => {
+            isLoggedIn = false;
+            closeProfileDropdown();
+            renderProfileMenu();
+        });
+    } else {
+        profileDropdown.innerHTML = `
+        <li><a href="signin.html">Sign In</a></li>
+        `;
+    }
+}
+
+function toggleProfileDropdown() {
+    profileDropdown.classList.toggle("open");
+}
+
+function closeProfileDropdown() {
+    profileDropdown.classList.remove("open");
+}
+
+profileBtn?.addEventListener("click", (e) => {
+    e.stopPropagation();
+    toggleProfileDropdown();
+});
+
+document.addEventListener("click", (e) => {
+    if (!profileMenu?.contains(e.target)) {
+        closeProfileDropdown();
+    }
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape")
+        closeProfileDropdown();
+});
+
+renderProfileMenu();
+
+const orgLink = document.querySelector('a[href="#organizations"]');
+const orgSection = document.getElementById("organizations");
+
+if (orgLink && orgSection) {
+  orgLink.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    orgSection.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+}
+// END OF MENU
